09e9b66ded25838dbc372a31e62d2f2f790da658
Ali <ali_modir2000@yahoo.com>
2021-08-14 23:06:28 +0430

    BasketApi Completed

---
+++ src/Services/Basket/Basket.API/Controllers/BasketController.cs
@@ -0,0 +1,50 @@
+﻿using Basket.API.Entities;
+using Basket.API.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Basket.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BasketController : ControllerBase
+    {
+        private readonly IBasketRepository _basketRepository;
+
+        public BasketController(IBasketRepository basketRepository)
+        {
+            _basketRepository = basketRepository;
+        }
+
+        [HttpGet("{username}", Name = "GetBasket")]   
+        [ProducesResponseType(typeof(ShoppingCart), (int)HttpStatusCode.OK )]
+        public async Task<ActionResult<ShoppingCart>> GetBasket(string username)
+        {
+            var basket = await _basketRepository.GetBasket(username);
+            return Ok(basket ?? new ShoppingCart(username));
+
+        }
+
+
+
+        [HttpPost]
+        public async Task<ActionResult<ShoppingCart>> UpdateBasket([FromBody] ShoppingCart basket)
+        {
+            var t = await _basketRepository.UpdateBasket(basket);
+            return Ok(t);
+
+        }
+
+        [HttpDelete]
+        public async Task DeleteBasket(string username)
+        {
+            await _basketRepository.DeleteBasket(username);            
+
+        }
+    }
+}
---
+++ src/Services/Basket/Basket.API/Entities/ShoppingCartItem.cs
@@ -0,0 +1,12 @@
+﻿namespace Basket.API.Entities
+{
+    public class ShoppingCartItem
+    {
+        public int Quntity { get; set; }
+        public decimal Price { get; set; }
+        public string Color { get; set; }
+        public string ProductID { get; set; }
+        public string ProductName { get; set; }
+
+    }
+}
