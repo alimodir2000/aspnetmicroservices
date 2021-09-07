fe73f45e9874c4b962fa635a5019b3b15c3d1db3
Ali <ali_modir2000@yahoo.com>
2021-09-07 18:01:35 +0430

    Abstaract implementation completed for Order.API

---
+++ src/Services/Ordering/Ordering.Application/Behaviours/ValidationBeharvior.cs
@@ -0,0 +1,38 @@
+﻿using FluentValidation;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using ValidationException = Ordering.Application.Exceptions.ValidationExceptions;
+
+namespace Ordering.Application.Behaviours
+{
+    public class ValidationBeharvior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+    {
+        private readonly IEnumerable<IValidator<TRequest>> _validators;
+
+        public ValidationBeharvior(IEnumerable<IValidator<TRequest>> validators)
+        {
+            _validators = validators;
+        }
+
+        public async Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken,
+            RequestHandlerDelegate<TResponse> next)
+        {
+            if (_validators.Any())
+            {
+                var context = new ValidationContext<TRequest>(request);
+                var validationResult =
+                    await Task.WhenAll(_validators.Select(x => x.ValidateAsync(context, cancellationToken)));
+
+                var failures = validationResult.SelectMany(x => x.Errors).Where(y => y != null).ToList();
+                if (failures.Count != 0)
+                    throw new ValidationException(failures);
+            }
+            return await next();
+        }
+    }
+}
---
+++ src/Services/Ordering/Ordering.Application/Exceptions/ValidationExceptions.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Exceptions
+{
+    public class ValidationExceptions : ApplicationException
+    {
+        public IDictionary<string, string[]> Errors { get; set; }
+
+        public ValidationExceptions()
+            :base("One or more validation failures have occured!")
+        {
+            Errors = new Dictionary<string, string[]>();
+        }
+
+        public ValidationExceptions(IEnumerable<ValidationFailure> errors)
+        {
+            Errors = errors.GroupBy(x => x.PropertyName, x => x.ErrorMessage)
+                .ToDictionary(y => y.Key, y => y.ToArray());
+        }
+    }
+}
